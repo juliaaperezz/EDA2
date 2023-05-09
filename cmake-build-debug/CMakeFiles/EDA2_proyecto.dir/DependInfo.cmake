2a3af61153cea7156ff69fb3fbd2c524cf9dad8d
carlanunezvinas <carla.nunez02@estudiant.upf.edu>
2023-05-09 13:30:33 +0200

    Carla - creación función menu

---
+++ cmake-build-debug/CMakeFiles/EDA2_proyecto.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/carlanunezvinas/CLionProjects/EDA2/functions.c" "CMakeFiles/EDA2_proyecto.dir/functions.c.o" "gcc" "CMakeFiles/EDA2_proyecto.dir/functions.c.o.d"
+  "/Users/carlanunezvinas/CLionProjects/EDA2/main.c" "CMakeFiles/EDA2_proyecto.dir/main.c.o" "gcc" "CMakeFiles/EDA2_proyecto.dir/main.c.o.d"
+  "/Users/carlanunezvinas/CLionProjects/EDA2/menu.c" "CMakeFiles/EDA2_proyecto.dir/menu.c.o" "gcc" "CMakeFiles/EDA2_proyecto.dir/menu.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/EDA2_proyecto.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDA2_proyecto.dir/functions.c.o"
+  "CMakeFiles/EDA2_proyecto.dir/functions.c.o.d"
+  "CMakeFiles/EDA2_proyecto.dir/main.c.o"
+  "CMakeFiles/EDA2_proyecto.dir/main.c.o.d"
+  "CMakeFiles/EDA2_proyecto.dir/menu.c.o"
+  "CMakeFiles/EDA2_proyecto.dir/menu.c.o.d"
+  "EDA2_proyecto"
+  "EDA2_proyecto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EDA2_proyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

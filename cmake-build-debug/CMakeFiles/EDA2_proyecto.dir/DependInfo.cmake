fe24ed5176bf0979fae8497bb846a5dd0cc93780
carlanunez <131855919+carlanunez@users.noreply.github.com>
2023-06-03 14:09:04 +0200

    Changes by dipti

---
+++ cmake-build-debug/CMakeFiles/EDA2_proyecto.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/carlanunezvinas/Documents/GitHub/EDA2/sources/src/funciones submenu.c" "CMakeFiles/EDA2_proyecto.dir/sources/src/funciones_submenu.c.o" "gcc" "CMakeFiles/EDA2_proyecto.dir/sources/src/funciones_submenu.c.o.d"
+  "/Users/carlanunezvinas/Documents/GitHub/EDA2/sources/src/main.c" "CMakeFiles/EDA2_proyecto.dir/sources/src/main.c.o" "gcc" "CMakeFiles/EDA2_proyecto.dir/sources/src/main.c.o.d"
+  "/Users/carlanunezvinas/Documents/GitHub/EDA2/sources/src/menu.c" "CMakeFiles/EDA2_proyecto.dir/sources/src/menu.c.o" "gcc" "CMakeFiles/EDA2_proyecto.dir/sources/src/menu.c.o.d"
+  "/Users/carlanunezvinas/Documents/GitHub/EDA2/sources/src/usuario.c" "CMakeFiles/EDA2_proyecto.dir/sources/src/usuario.c.o" "gcc" "CMakeFiles/EDA2_proyecto.dir/sources/src/usuario.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

dd980131d9a8eee14417f5a6948650767952e281
DiptiNiroula <dipti.niroula01@estudiant.upf.edu>
2023-06-06 10:58:55 +0200

    aaaaaaaaa

---
+++ cmake-build-debug/CMakeFiles/EDA2_proyecto.dir/sources/src/document_r.c.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/EDA2_proyecto.dir/sources/src/document_r.c.obj: \
+ C:\Users\dipti\Documents\GitHub\EDA2\sources\src\document\ r.c
---
+++ cmake-build-debug/CMakeFiles/EDA2_proyecto.dir/sources/src/documento_2.c.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/EDA2_proyecto.dir/sources/src/documento_2.c.obj: \
+ C:\Users\dipti\Documents\GitHub\EDA2\sources\src\documento\ 2.c
